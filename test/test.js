77b6a94caa15bcd5501106edb3072f6c02b3c232
Justas Brazauskas <brazauskasjustas@gmail.com>
2013-10-10 17:04:01 +0300

    added timeout for tests because of server startUp

--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@ var exec = require('child_process').exec;
 var expect = require('expect.js');
 var helper = require('./helper');
 var git_server = require('../host');
+
 var test_octocat_name = helper.random();
 var test_repo_name = helper.random();
 var server;
@@ -34,116 +35,118 @@ var opts = {
 	repoLocation: '/tmp/'+helper.random(),
 	port: 8000
 };
+
 server = new git_server(opts.repos, opts.logging, opts.repoLocation, opts.port);
 
-describe('git_server', function() {
-	it('Should expose a function', function() {
-		expect(git_server).to.be.a('function');
-	});
+setTimeout(function() {
+	describe('git_server', function() {
+		it('Should expose a function', function() {
+			expect(git_server).to.be.a('function');
+		});
 
-	describe('server', function() {
-		it('Should be an object', function() {
-			expect(server).to.be.an('object');
-		});
-		describe('#repos', function() {
-			it('Should be an Array', function() {
-				expect(server.repos).to.be.an('array');
-			});
-		});
-		describe('#logging', function() {
-			it('Should be a boolean', function() {
-				expect(server.logging).to.be.a('boolean');
-			});
-		});
-		describe('#repoLocation', function() {
-			it('Should be a string equals to '+opts.repoLocation, function() {
-				expect(server.repoLocation).to.be.a('string').and.to.be.equal(opts.repoLocation);
-			});
-		});
-		describe('#port', function() {
-			it('Should be an integer equals to '+opts.port, function() {
-				expect(server.port).to.be.a('number').and.to.be.equal(opts.port);
-			});
-		});
-		describe('#on()', function() {
-			it('Should be a function', function() {
-				expect(server.on).to.be.a('function');
-			});
-		});
-		describe('#getRepo()', function() {
-			it('Should be a function and return repo object', function() {
-				expect(server.getRepo).to.be.a('function');
-				expect(server.getRepo(repo.name+".git")).to.be.an('object').and.to.have.keys(['name', 'anonRead', 'users', ]);
-			});
-		});
-		describe('#getUser()', function() {
-			it('Should be a function and return user object', function() {
-				expect(server.getUser).to.be.a('function');
-				expect(server.getUser(user.username, user.password, repo)).to.be.an('object').and.to.have.keys(['user']);
-				expect(server.getUser(user.username, user.password, repo).user).to.be.an('object').and.to.have.keys(['username', 'password']);
-			});
-		});
-		describe('#checkTriggers()', function() {
-			it('Should be a function', function() {
-				expect(server.checkTriggers).to.be.a('function');
-			});
-		});
-		describe('#onPush()', function() {
-			it('Should be a function', function() {
-				expect(server.onPush).to.be.a('function');
-			});
-		});
-		describe('#onFetch()', function() {
-			it('Should be a function', function() {
-				expect(server.onFetch).to.be.a('function');
-			});
-		});
-		describe('#makeReposIfNull()', function() {
-			it('Should be a function', function() {
-				expect(server.makeReposIfNull).to.be.a('function');
-			});
-		});
-		describe('#gitListeners()', function() {
-			it('Should be a function', function() {
-				expect(server.gitListeners).to.be.a('function');
-			});
-		});
-		describe('#permissableMethod()', function() {
-			it('Should be a function', function() {
-				expect(server.permissableMethod).to.be.a('function');
-			});
-		});
-		describe('#processSecurity()', function() {
-			it('Should be a function', function() {
-				expect(server.processSecurity).to.be.a('function');
-			});
-		});
-		describe('#log()', function() {
-			it('Should be a function', function() {
-				expect(server.log).to.be.a('function');
-			});
-		});
-		describe('#createRepo()', function() {
-			it('Should be a function', function() {
-				expect(server.createRepo).to.be.a('function');
-			});
-		});
-		describe('#git', function() {
+		describe('server', function() {
 			it('Should be an object', function() {
-				expect(server.git).to.be.an('object').and.to.have.keys(['dirMap', 'autoCreate', 'checkout']);
+				expect(server).to.be.an('object');
+			});
+			describe('#repos', function() {
+				it('Should be an Array', function() {
+					expect(server.repos).to.be.an('array');
+				});
+			});
+			describe('#logging', function() {
+				it('Should be a boolean', function() {
+					expect(server.logging).to.be.a('boolean');
+				});
+			});
+			describe('#repoLocation', function() {
+				it('Should be a string equals to '+opts.repoLocation, function() {
+					expect(server.repoLocation).to.be.a('string').and.to.be.equal(opts.repoLocation);
+				});
+			});
+			describe('#port', function() {
+				it('Should be an integer equals to '+opts.port, function() {
+					expect(server.port).to.be.a('number').and.to.be.equal(opts.port);
+				});
+			});
+			describe('#on()', function() {
+				it('Should be a function', function() {
+					expect(server.on).to.be.a('function');
+				});
+			});
+			describe('#getRepo()', function() {
+				it('Should be a function and return repo object', function() {
+					expect(server.getRepo).to.be.a('function');
+					expect(server.getRepo(repo.name+".git")).to.be.an('object').and.to.have.keys(['name', 'anonRead', 'users', ]);
+				});
+			});
+			describe('#getUser()', function() {
+				it('Should be a function and return user object', function() {
+					expect(server.getUser).to.be.a('function');
+					expect(server.getUser(user.username, user.password, repo)).to.be.an('object').and.to.have.keys(['user']);
+					expect(server.getUser(user.username, user.password, repo).user).to.be.an('object').and.to.have.keys(['username', 'password']);
+				});
+			});
+			describe('#checkTriggers()', function() {
+				it('Should be a function', function() {
+					expect(server.checkTriggers).to.be.a('function');
+				});
+			});
+			describe('#onPush()', function() {
+				it('Should be a function', function() {
+					expect(server.onPush).to.be.a('function');
+				});
+			});
+			describe('#onFetch()', function() {
+				it('Should be a function', function() {
+					expect(server.onFetch).to.be.a('function');
+				});
+			});
+			describe('#makeReposIfNull()', function() {
+				it('Should be a function', function() {
+					expect(server.makeReposIfNull).to.be.a('function');
+				});
+			});
+			describe('#gitListeners()', function() {
+				it('Should be a function', function() {
+					expect(server.gitListeners).to.be.a('function');
+				});
+			});
+			describe('#permissableMethod()', function() {
+				it('Should be a function', function() {
+					expect(server.permissableMethod).to.be.a('function');
+				});
+			});
+			describe('#processSecurity()', function() {
+				it('Should be a function', function() {
+					expect(server.processSecurity).to.be.a('function');
+				});
+			});
+			describe('#log()', function() {
+				it('Should be a function', function() {
+					expect(server.log).to.be.a('function');
+				});
+			});
+			describe('#createRepo()', function() {
+				it('Should be a function', function() {
+					expect(server.createRepo).to.be.a('function');
+				});
+			});
+			describe('#git', function() {
+				it('Should be an object', function() {
+					expect(server.git).to.be.an('object').and.to.have.keys(['dirMap', 'autoCreate', 'checkout']);
+				});
+			});
+			describe('#permMap', function() {
+				it('Should be an object', function() {
+					expect(server.permMap).to.be.an('object').and.to.be.eql({ fetch: 'R', push: 'W' });
+				});
+			});
+			describe('#server', function() {
+				it('Should be an object', function() {
+					expect(server.server).to.be.an('object');
+				});
 			});
 		});
-		describe('#permMap', function() {
-			it('Should be an object', function() {
-				expect(server.permMap).to.be.an('object').and.to.be.eql({ fetch: 'R', push: 'W' });
-			});
-		});
-		describe('#server', function() {
-			it('Should be an object', function() {
-				expect(server.server).to.be.an('object');
-			});
-		});
-	});
 
 describe('behaviour', function() {
 	describe('Clone a Spoon-Knife repo', function() {
@@ -330,3 +333,4 @@ describe('Clone', function() {
 });
 });
 });
+}, 300);
